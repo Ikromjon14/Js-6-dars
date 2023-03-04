fd0df412d24c3071fadf8bed73efb557bcc398d3
ikromjon14 <abduraxmonovikromjon667@gmail.com>
2023-03-04 12:06:32 +0500

    Finish codes

--- js/main.js
+++ js/main.js
@@ -115,16 +115,24 @@
 // var num = 10
 // num--
 // console.log(num);
+// Pulni so'rash
+var surashh2 = document.querySelector("h2");
+var sum = +prompt("Mablag'ingizni kiriting");
+// aqsh pullari
 var plainCostdollor = 500;
 var hotelCostdollor = 250;
 var meseumCostEuro = 120;
-
+// o'zbek pulllari
 var usdToSum = 11349.99;
-var usdToSum = 12078.66;
-
-var totalDollor = (plainCostdollor + hotelCostdollor) * meseumCostEuro * usdToSum;
-var totalEuro = meseumCostEuro * evroToSum;
+var euroToSum = 12078.66;
+// mablag'larni yig'ish
+var totalDollor = (plainCostdollor + hotelCostdollor) * usdToSum;
+var totalEuro = meseumCostEuro * euroToSum;
 var totalSum = totalDollor + totalEuro;
 console.log(totalSum);
-
-var sum = +prompt("Mablag'ingizni kiriting")
+// maslahat berish
+if(totalSum <= sum) {
+  surashh2.textContent = "Oq yul"
+} else {
+  surashh2.textContent = "Uyda utirib kino ko'ring"
+}
