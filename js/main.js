0e5794eeb44f0b5eb0aba15635a998b35ccb14b4
ikromjon14 <abduraxmonovikromjon667@gmail.com>
2023-03-07 11:35:49 +0500

    js kinish

--- js/main.js
+++ js/main.js
@@ -116,23 +116,31 @@
 // num--
 // console.log(num);
 // Pulni so'rash
-var surashh2 = document.querySelector("h2");
-var sum = +prompt("Mablag'ingizni kiriting");
-// aqsh pullari
-var plainCostdollor = 500;
-var hotelCostdollor = 250;
-var meseumCostEuro = 120;
-// o'zbek pulllari
-var usdToSum = 11349.99;
-var euroToSum = 12078.66;
-// mablag'larni yig'ish
-var totalDollor = (plainCostdollor + hotelCostdollor) * usdToSum;
-var totalEuro = meseumCostEuro * euroToSum;
-var totalSum = totalDollor + totalEuro;
-console.log(totalSum);
-// maslahat berish
-if(totalSum <= sum) {
-  surashh2.textContent = "Oq yul"
-} else {
-  surashh2.textContent = "Uyda utirib kino ko'ring"
+
+
+
+// var surashh2 = document.querySelector("h2");
+// var sum = +prompt("Mablag'ingizni kiriting");
+// // aqsh pullari
+// var plainCostdollor = 500;
+// var hotelCostdollor = 250;
+// var meseumCostEuro = 120;
+// // o'zbek pulllari
+// var usdToSum = 11349.99;
+// var euroToSum = 12078.66;
+// // mablag'larni yig'ish
+// var totalDollor = (plainCostdollor + hotelCostdollor) * usdToSum;
+// var totalEuro = meseumCostEuro * euroToSum;
+// var totalSum = totalDollor + totalEuro;
+// console.log(totalSum);
+// // maslahat berish
+// if(totalSum <= sum) {
+//   surashh2.textContent = "Oq yul"
+// } else {
+//   surashh2.textContent = "Uyda utirib kino ko'ring"
+// }
+var tanlang = document.querySelector("son tanlang")
+var five = ("5")
+if (five > 10) {
+
 }
