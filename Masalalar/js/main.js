c1e1a2c98f73ff2afe7e3df8c23d9f05ddf54619
ikromjon14 <abduraxmonovikromjon667@gmail.com>
2023-03-09 11:24:45 +0500

    Finish projekt

---
+++ Masalalar/js/main.js
@@ -0,0 +1,37 @@
+          // 1-masala
+// var raqam = +prompt("Raqam tanlang");
+// var ten = ("10");
+// if (raqam > ten) {
+//   console.log(raqam + 3);
+// } else {
+//   console.log(raqam * 2);
+// }
+            // 2-masala
+// var a = +prompt("Raqam");
+
+// var b = Math.round(a / 3);
+
+// console.log(b);
+              //  3-masala
+// var num = +prompt("Raqam")
+// var son = 3 
+// console.log(num % son);
+
+              //  4-masala
+// var raqam = +prompt("Raqam tanlang") 
+// var bulinma = (raqam % 2)
+// if (bulinma == 0 ) {
+//   console.log("juft");
+// } else {
+//   console.log("toq");
+// }
+                  // 5-masala
+var tanlang = +prompt("son tanlang")
+var tanlangikki = +prompt("ikkinchisini tanlang")
+if (tanlang > tanlangikki) {
+  console.log(tanlang);
+} else {
+  console.log(tanlangikki);
+} else if {
+  console.log("Bir xil ekan");
+}
